f81f75328efdd8f2702e888f70921632f3e579b2
muhammed.poyil <muhammed.poyil@dewa.gov.ae>
2016-09-03 18:10:27 +0400

    Deleted unused classes

--- app/src/main/java/com/carlorry/activity/SplashActivity.java
+++ app/src/main/java/com/carlorry/activity/SplashActivity.java
@@ -27,7 +27,7 @@ public class SplashActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                    Intent intent = new Intent(SplashActivity.this, LauchActivity.class);
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                     startActivity(intent);
                 }
             }
