0e2693fcea3e2e63243cb75b054465047a5787d5
muhammed.poyil <muhammed.poyil@dewa.gov.ae>
2016-09-06 20:30:32 +0400

    files changed

--- app/src/main/java/com/carlorry/fragments/CitySelectionFragment.java
+++ app/src/main/java/com/carlorry/fragments/CitySelectionFragment.java
@@ -4,22 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.Button;
 
-import com.carlorry.Utils.Constants;
 import com.carlorry.activity.HomeActivity;
 import com.carlorry.activity.R;
-import com.carlorry.activity.TripSelectionActivity;
 
 /**
  * Created by muhammed.poyil on 9/3/2016.
  */
-public class CitySelectionFragment extends DialogFragment {
+public class CitySelectionFragment extends DialogFragment implements View.OnClickListener {
     private static final String ARG_SECTION_Id = "section_id";
     private int selCountry;
 
@@ -35,18 +31,29 @@ public class CitySelectionFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         selCountry = getArguments().getInt(ARG_SECTION_Id);
-        View rootView = inflater.inflate(R.layout.fragment_select_india_loc, container);
-        if (selCountry == 0)
+        View rootView;
+        if (selCountry == 0) {
+            rootView = inflater.inflate(R.layout.fragment_select_india_loc, container);
+        } else {
             rootView = inflater.inflate(R.layout.fragment_select_uaeloc, container);
-
-        ((Button) rootView.findViewById(R.id.btn_done)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getActivity(), HomeActivity.class);
-                startActivity(intent);
-            }
-        });
+        }
+        ((Button) rootView.findViewById(R.id.btn_done)).setOnClickListener(this);
 
         return rootView;
+    }
+
+    private void intializeViews(View rootView) {
+        //Bangloor
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_done:
+                Intent intent = new Intent(getActivity(), HomeActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/carlorry/fragments/CountrySelectionFragment.java
+++ app/src/main/java/com/carlorry/fragments/CountrySelectionFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.carlorry.Utils.Constants;
 import com.carlorry.activity.R;
@@ -16,7 +17,9 @@ import com.carlorry.activity.R;
 /**
  * Created by muhammed.poyil on 9/3/2016.
  */
-public class CountrySelectionFragment extends DialogFragment {
+public class CountrySelectionFragment extends DialogFragment implements View.OnClickListener {
+    private RadioButton rbIndia, rbUAE;
+    private int selCountry = 0;
 
     public static CountrySelectionFragment newInstance() {
         return new CountrySelectionFragment();
@@ -26,15 +29,36 @@ public class CountrySelectionFragment extends DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_select_country, container);
-        ((Button) rootView.findViewById(R.id.btn_done)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        ((Button) rootView.findViewById(R.id.btn_done)).setOnClickListener(this);
+
+        rbIndia = (RadioButton) rootView.findViewById(R.id.rb_india);
+        rbUAE = (RadioButton) rootView.findViewById(R.id.rb_uae);
+        rbIndia.setOnClickListener(this);
+        rbUAE.setOnClickListener(this);
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_done:
                 FragmentManager fm = getActivity().getSupportFragmentManager();
-                CitySelectionFragment citySelectionFragment = CitySelectionFragment.newInstance(0);
+                CitySelectionFragment citySelectionFragment = CitySelectionFragment.newInstance(selCountry);
                 citySelectionFragment.show(fm, Constants.KEY_CitySelection);
                 dismiss();
-            }
-        });
-        return rootView;
+                break;
+            case R.id.rb_india:
+                selCountry = 0;
+                rbUAE.setChecked(false);
+//                rbUAE.setButtonDrawable(R.drawable.radio_blank);
+                break;
+            case R.id.rb_uae:
+                selCountry = 1;
+                rbIndia.setChecked(false);
+//                rbIndia.setButtonDrawable(R.drawable.radio_blank);
+                break;
+        }
     }
 }
